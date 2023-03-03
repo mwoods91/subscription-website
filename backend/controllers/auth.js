2b7f1429b3708e9c32cf942a08a2ee2cc4b74521
Mark Woods <91460370+mwoods91@users.noreply.github.com>
2023-03-03 10:32:24 +0000

    SMS added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,11 @@
 const User = require("../models/user");
 const EmailVerificationToken = require("../models/emailVerificationToken");
-const Record = require("../models/record");
-const { isValidObjectId } = require("mongoose");
 const { sendError } = require("../utils/helper");
-// const { hashPassword, comparePassword } = require("../helpers/auth");
 const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const request = require("request");
 
 exports.register = async (req, res) => {
   try {
@@ -56,89 +54,6 @@ exports.register = async (req, res) => {
     console.log(err);
   }
 };
-//   try {
-//     // validation
-//     const { client_reference_id, fullname, dob, email, phone } = req.body;
-//     if (!email) {
-//       return res.json({
-//         error: "Name is required",
-//       });
-//     }
-//     const exist = await User.findOne({ email });
-//     if (exist) {
-//       return res.json({
-//         error: "Email is taken",
-//       });
-//     }
-
-//     console.log(email);
-
-//     // create account in stripe
-//     const customer = await stripe.customers.create({
-//       metadata: {
-//         client_reference_id,
-//       },
-//       email: email,
-//       name: fullname,
-//     });
-
-//     // console.log("stripe customer created on signup", customer);
-
-//     try {
-//       const user = await new User({
-//         client_reference_id,
-//         fullname,
-//         dob,
-//         email,
-//         phone,
-//         stripe_customer_id: customer.id,
-//       }).save();
-
-//       const record = await new Record({
-//         patient_record: user._id,
-//       }).save();
-
-//       // create signed token
-//       const confirmtoken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-//         expiresIn: "7d",
-//       });
-
-//       //   console.log(user);
-//       const { ...rest } = user._doc;
-//       console.log(...rest);
-//       return res.json({
-//         // confirmtoken,
-//         confirmtoken,
-//         record: rest,
-//         user: rest,
-//       });
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// exports.login = async (req, res) => {
-//   const user = await User.findOne({ email: req.body.email });
-//   if (!user) return sendError(res, "Mobile Not Found!");
-
-//   // const matched = await comparePassword(req.body.password, user.password);
-//   // if (!matched) return sendError(res, "Email/Password mismatch!");
-
-//   // create signed token
-//   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-//     expiresIn: "1d",
-//   });
-
-//   const { ...rest } = user._doc;
-
-//   res.json({
-//     token,
-//     user: rest,
-//   });
-// };
 
 exports.sendOTP = async (req, res) => {
   const { email } = req.body;
@@ -149,6 +64,24 @@ exports.sendOTP = async (req, res) => {
   // Generate a random 6-digit OTP code
   const otpCode = Math.floor(100000 + Math.random() * 900000);
 
+  var options = {
+    method: "POST",
+    url: "https://api.releans.com/v2/message",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImZmZDZlN2E2LTU3MzAtNDFiZi1iMTM3LTNjMjEwNDhhNmJkYiIsImlhdCI6MTY3MTczOTk2NCwiaXNzIjoxNTE4NX0.5rqDT4bLY6VipJ8RnKWw04fe7Flamxbhpk9CMskUHqI",
+    },
+    form: {
+      sender: "heartrhythm",
+      mobile: user.phone.replace("0", "+353"),
+      content: `Your OTP code is ${otpCode}`,
+    },
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
+
   const newEmailVerificationToken = new EmailVerificationToken({
     owner: user,
     token: otpCode,
@@ -156,23 +89,24 @@ exports.sendOTP = async (req, res) => {
 
   await newEmailVerificationToken.save();
 
-  const msg = {
-    to: user.email, // recipient email address
-    from: "markwoodsy1991@gmail.com", // sender email address
-    subject: "Your OTP code", // email subject
-    text: `Your OTP code is ${otpCode}.`, // plain text body of the email
-  };
+  // const msg = {
+  //   to: user.email, // recipient email address
+  //   from: "markwoodsy1991@gmail.com", // sender email address
+  //   subject: "Your OTP code", // email subject
+  //   text: `Your OTP code is ${otpCode}.`, // plain text body of the email
+  // };
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log(`OTP code sent to ${msg.to}`);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log(`OTP code sent to ${msg.to}`);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 
   res.json({
+    request,
     otpCode,
     // user: rest,
   });
